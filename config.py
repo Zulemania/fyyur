c638047e945b01e85251aa11183f9250a2827233
Asiegbu-Cynthia-Chisom <asiegbucynthia.cc@gmail.com>
2021-04-29 21:06:35 +0100

    working version

--- config.py
+++ config.py
@@ -10,4 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://cynthiachisom@localhost:5432/fyyur'
+SQALCHEMY_TRACK_MODIFICATIONS = False
